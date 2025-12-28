592a1366fd2c6b8e2a52d2869bb529174fc66563
Brannegan <zapp.brannegan@gmail.com>
2025-12-28 20:41:49 +0700

    AOC-2025. day2 part2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -2,6 +2,8 @@ use std::fs::read_to_string;
 use std::ops::RangeInclusive;
 
 use anyhow::{Ok, Result, anyhow};
+use itertools::Itertools;
+use num::Integer;
 
 fn parse(input: &str) -> Result<Vec<RangeInclusive<u64>>> {
     input
@@ -14,31 +16,38 @@ fn parse(input: &str) -> Result<Vec<RangeInclusive<u64>>> {
         })
         .collect()
 }
-
-fn invalid_pattern_part1(range: &RangeInclusive<u64>) -> impl Iterator<Item=u64> {
-    range
-        .clone()
-        .filter(|num| {
-            let num_as_str = num.to_string();
-            num_as_str.len() % 2 == 0 &&
-            num_as_str[..num_as_str.len() / 2] == num_as_str[num_as_str.len() / 2..]
-        })
+fn is_invalid_id_part1(id: &u64) -> bool {
+    let id = id.to_string();
+    id.len().is_multiple_of(2) && id[..id.len() / 2] == id[id.len() / 2..]
 }
-fn invalid_ids_sum<'a,F,R>(ranges: &'a[RangeInclusive<u64>], func: F) -> u64
-where
-    R: Iterator<Item = u64>,
-    F: Fn(&'a RangeInclusive<u64>) -> R,
-{
-    ranges.iter().flat_map(func).sum()
+fn is_invalid_id_part2(id: &u64) -> bool {
+    if is_invalid_id_part1(id) {
+        return true
+    }
+    let id = id.to_string();
+    let char_stats = id.chars().counts();
+    let min_val = char_stats.values().min().copied().expect("char_stats not empty");
+    let chunk_size = id.len().gcd(&min_val);
+    if chunk_size == 1 { return false }
+    id.as_bytes().chunks(id.len() / chunk_size).all_equal()
+}
+
+fn invalid_ids_sum(ranges: &[RangeInclusive<u64>], pred: fn(&u64) -> bool) -> u64 {
+    ranges
+        .iter()
+        .flat_map(|range| {
+            range.clone().filter(pred)
+        })
+        .sum()
 }
 
 fn main() -> Result<()> {
     let input = read_to_string("inputs/day02-input1.txt")?;
     let ranges = parse(input.trim())?;
-    let answer = invalid_ids_sum(&ranges,invalid_pattern_part1);
+    let answer = invalid_ids_sum(&ranges, is_invalid_id_part1);
     println!("part 1 answer is: {answer}");
-    //let answer = count_dials_crosses_zero(input.trim(), 50)?;
-    //println!("part 2 answer is: {answer}");
+    let answer2 = invalid_ids_sum(&ranges, is_invalid_id_part2);
+    println!("part 2 answer is: {answer2}");
     Ok(())
 }
 #[cfg(test)]
@@ -49,20 +58,15 @@ mod tests {
 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
 "#;
     #[test]
-    fn part1_invalid_11_22() -> Result<()> {
-        let ranges = parse(INPUT.trim())?;
-        assert_eq!(invalid_pattern_part1(&ranges[0]).sum::<u64>(), 11 + 22);
-        Ok(())
-    }
-    #[test]
     fn part1() -> Result<()> {
         let ranges = parse(INPUT.trim())?;
-        assert_eq!(invalid_ids_sum(&ranges,invalid_pattern_part1), 1227775554);
+        assert_eq!(invalid_ids_sum(&ranges, is_invalid_id_part1), 1227775554);
         Ok(())
     }
     #[test]
     fn part2() -> Result<()> {
-        assert_eq!(6, 6);
+        let ranges = parse(INPUT.trim())?;
+        assert_eq!(invalid_ids_sum(&ranges, is_invalid_id_part2), 4174379265);
         Ok(())
     }
 }
