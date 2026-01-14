debda002237343de64316c22e0ecff3491f53381
Brannegan <zapp.brannegan@gmail.com>
2026-01-14 08:22:36 +0700

    AOC-2025. day9 part2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,14 +1,15 @@
 use anyhow::{Ok, Result};
-use glam::I64Vec2;
+use glam::IVec2;
+use std::cmp::Ordering;
 use std::fs::read_to_string;
+use std::ops::Range;
 
 #[derive(Debug, Clone)]
 struct RedTileList {
-    coords: Vec<I64Vec2>,
+    coords: Vec<IVec2>,
 }
-
 impl RedTileList {
-    fn new(coords: Vec<I64Vec2>) -> Self {
+    fn new(coords: Vec<IVec2>) -> Self {
         Self { coords }
     }
     fn max_area(&self) -> i64 {
@@ -16,13 +17,106 @@ impl RedTileList {
         let len = self.coords.len();
         for i in 0..len {
             for j in i + 1..len {
-                let area = ((self.coords[i].x - self.coords[j].x).abs() + 1)
-                    * ((self.coords[i].y - self.coords[j].y).abs() + 1);
+                let area = ((self.coords[i].x - self.coords[j].x).abs() + 1) as i64
+                    * ((self.coords[i].y - self.coords[j].y).abs() + 1) as i64;
                 max_area = max_area.max(area);
             }
         }
         max_area
     }
+    fn max_area_in_polygon(&self) -> i64 {
+        let mut max_area = 0;
+        let len = self.coords.len();
+        for i in 0..len {
+            for j in i + 1..len {
+                let area = ((self.coords[i].x - self.coords[j].x).abs() + 1) as i64
+                    * ((self.coords[i].y - self.coords[j].y).abs() + 1) as i64;
+                if max_area > area {
+                    continue;
+                }
+                if !self.is_smaller_rect_in_polygon(i, j) {
+                    continue;
+                }
+                max_area = area;
+            }
+        }
+        max_area
+    }
+    fn is_intersects_edge(&self, edge: [IVec2; 2]) -> bool {
+        self.coords
+            .iter()
+            .zip(self.coords.iter().skip(1))
+            .map(|(a,b)| [*a,*b])
+            .any(|poly_edge| {
+                let (ax, ay) = ranges_from_edge(edge);
+                let (bx, by) = ranges_from_edge(poly_edge);
+                ax.contains(&bx.start) && by.contains(&ay.start)
+                    || ay.contains(&by.start) && bx.contains(&ax.start)
+            })
+    }
+    fn is_smaller_rect_in_polygon(&self, i: usize, j: usize) -> bool {
+        let sign_x = (self.coords[i].x - self.coords[j].x).signum();
+        let sign_y = (self.coords[i].y - self.coords[j].y).signum();
+        let edge_ix = [
+            IVec2::new(self.coords[i].x - sign_x, self.coords[i].y - sign_y),
+            IVec2::new(self.coords[j].x + sign_x, self.coords[i].y - sign_y),
+        ];
+        let edge_iy = [
+            IVec2::new(self.coords[i].x - sign_x, self.coords[i].y - sign_y),
+            IVec2::new(self.coords[i].x - sign_x, self.coords[j].y + sign_y),
+        ];
+        let edge_jx = [
+            IVec2::new(self.coords[j].x + sign_x, self.coords[j].y + sign_y),
+            IVec2::new(self.coords[i].x - sign_x, self.coords[j].y + sign_y),
+        ];
+        let edge_jy = [
+            IVec2::new(self.coords[j].x + sign_x, self.coords[j].y + sign_y),
+            IVec2::new(self.coords[j].x + sign_x, self.coords[i].y - sign_y),
+        ];
+        [edge_ix, edge_iy, edge_jx, edge_jy]
+            .iter()
+            .all(|edge| !self.is_intersects_edge(*edge))
+    }
+    fn find_next_red_tile(&self, cur: IVec2) -> Option<IVec2> {
+        self.coords
+            .iter()
+            .copied()
+            .filter(|next| *next != cur)
+            .find(|next| cur.x == next.x || cur.y == next.y)
+    }
+    fn build_polygon(&mut self) {
+        let len = self.coords.len();
+        let mut polygon = Vec::with_capacity(len);
+        let upper_left = self
+            .coords
+            .iter()
+            .copied()
+            .min_by(|a, b| match a.x.cmp(&b.x) {
+                Ordering::Less => Ordering::Less,
+                Ordering::Greater => Ordering::Greater,
+                Ordering::Equal => a.y.cmp(&b.y),
+            })
+            .expect("empty list");
+        polygon.push(
+            self.coords
+                .remove(self.coords.iter().position(|c| *c == upper_left).unwrap()),
+        );
+        let mut cur = upper_left;
+        while let Some(next) = self.find_next_red_tile(cur) {
+            let remove_i = self.coords.iter().position(|c| *c == next).unwrap();
+            polygon.push(self.coords.remove(remove_i));
+            cur = next;
+        }
+        polygon.push(upper_left);
+        self.coords = polygon;
+    }
+}
+
+fn ranges_from_edge(edge: [IVec2; 2]) -> (Range<i32>, Range<i32>) {
+    let [a, b] = edge;
+    let px = if a.x < b.x { a.x..b.x } else { b.x..a.x };
+    let py = if a.y < b.y { a.y..b.y } else { b.y..a.y };
+    (px, py)
 }
 
 fn parse(input: &str) -> RedTileList {
@@ -30,7 +124,7 @@ fn parse(input: &str) -> RedTileList {
         .lines()
         .map(|line| {
             line.split_once(',')
-                .map(|(x, y)| I64Vec2::from((x.parse().unwrap(), y.parse().unwrap())))
+                .map(|(x, y)| IVec2::from((x.parse().unwrap(), y.parse().unwrap())))
                 .unwrap()
         })
         .collect();
@@ -39,12 +133,11 @@ fn parse(input: &str) -> RedTileList {
 
 fn main() -> Result<()> {
     let input = read_to_string("inputs/day09-input1.txt")?;
-    let red_tiles = parse(input.trim());
+    let mut red_tiles = parse(input.trim());
     let answer = red_tiles.max_area();
     println!("part 1 answer is: {answer}");
-
-    let red_tiles = parse(input.trim());
-    let answer = red_tiles.max_area();
+    red_tiles.build_polygon();
+    let answer = red_tiles.max_area_in_polygon();
     println!("part 2 answer is: {answer}");
     Ok(())
 }
@@ -72,8 +165,9 @@ mod tests {
     }
     #[test]
     fn part2() -> Result<()> {
-        let red_tiles = parse(INPUT.trim());
-        let answer = red_tiles.max_area();
+        let mut red_tiles = parse(INPUT.trim());
+        red_tiles.build_polygon();
+        let answer = red_tiles.max_area_in_polygon();
         assert_eq!(answer, 24);
         Ok(())
     }
