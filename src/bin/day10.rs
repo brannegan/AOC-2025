36c96a66ed51a0930726dbaedbbf52b984df8338
Brannegan <zapp.brannegan@gmail.com>
2026-01-28 10:42:45 +0700

    AOC-2025. day10 part2 lpsolver to the rescue

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,5 +1,6 @@
 use anyhow::{Ok, Result};
 use glam::bool;
+use lpsolve::Problem;
 use nom::bytes::complete::tag;
 use nom::character::complete::{line_ending, one_of, space1, usize};
 use nom::multi::{many1, separated_list0, separated_list1};
@@ -19,22 +20,6 @@ impl Button {
         for i in self.0.iter().copied() {
             lights_state[i] = !lights_state[i];
         }
-    }
-    fn press_counters(&self, mut counters: Vec<usize>) -> Option<(Vec<usize>, usize)> {
-        if self.0.iter().any(|i| counters[*i] == 0) {
-            return None;
-        }
-        let min_pressed = self
-            .0
-            .iter()
-            .copied()
-            .map(|btn_i| counters[btn_i])
-            .min()
-            .expect("min not found");
-        for i in self.0.iter().copied() {
-            counters[i] -= min_pressed;
-        }
-        Some((counters, min_pressed))
     }
 }
 
@@ -92,34 +77,23 @@ impl Machine {
         }
         min
     }
-    fn try_next_button(
-        &self,
-        button_i: usize,
-        counters: Vec<usize>,
-        pressed_so_far: usize,
-    ) -> Option<(Vec<usize>, usize)> {
-        let (new_counters, pressed_now) = self.buttons[button_i].press_counters(counters)?;
-        //  eprintln!(
-        //      "btn={button_i}, counters={:?}, {pressed_so_far}, {pressed_now}",
-        //      new_counters
-        // );
-        if new_counters == vec![0; self.joltage_req.len()] {
-            return Some((new_counters, pressed_so_far + pressed_now));
-        }
-        (0..self.buttons.len())
-            .filter(|&btn_i| btn_i != button_i)
-            .filter_map(|btn_i| {
-                self.try_next_button(btn_i, new_counters.clone(), pressed_so_far + pressed_now)
-            })
-            .min_by_key(|(_, count)| *count)
-    }
     fn min_buttons_joltage(&self) -> usize {
-        (0..self.buttons.len())
-            .filter_map(|button_i| self.try_next_button(button_i, self.joltage_req.clone(), 0))
-            //.inspect(|(counters, pressed)| eprintln!("counters {counters:?}, pressed={pressed}"))
-            .map(|(_, pressed)| pressed)
-            .min()
-            .unwrap()
+        let cols = self.buttons.len();
+        let mut problem = Problem::builder()
+            .cols(cols as i32)
+            .min(&vec![1.; cols])
+            .integer_vars(&(1..=cols as i32).collect::<Vec<_>>())
+            .non_negative_integers()
+            .verbosity(lpsolve::Verbosity::Critical);
+        for j in 0..self.joltage_req.len() {
+            let coeffs: Vec<_> = self
+                .buttons
+                .iter()
+                .map(|btn| if btn.0.contains(&j) { 1. } else { 0. }).collect();
+            problem = problem.eq(&coeffs, self.joltage_req[j] as f64);
+        }
+        let solution = problem.solve().expect("unable to solve lp problem");
+        solution.objective_value().ceil() as usize
     }
 }
 
@@ -151,28 +125,24 @@ fn parse(input: &str) -> anyhow::Result<Vec<Machine>> {
 }
 
 fn main() -> Result<()> {
-    //let input = read_to_string("inputs/day10-input1.txt")?;
-    //let mut machines = parse(input.trim())?;
-    //let answer = machines
-    //    .iter_mut()
-    //    .map(|machine| machine.min_buttons_seq())
-    //    //.inspect(|min| eprintln!("min={min}"))
-    //    .sum::<usize>();
-    //println!("part 1 answer is: {answer}");
     let input = read_to_string("inputs/day10-input1.txt")?;
-    let machines = parse(input.trim())?;
+    let mut machines = parse(input.trim())?;
+    let answer = machines
+        .iter_mut()
+        .map(|machine| machine.min_buttons_seq())
+        .sum::<usize>();
+    println!("part 1 answer is: {answer}");
     let answer = machines
         .iter()
         .map(|machine| machine.min_buttons_joltage())
-        //.inspect(|min| eprintln!("min={min}"))
         .sum::<usize>();
     println!("part 2 answer is: {answer}");
-    // println!("part 2 answer is: {answer}");
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     const INPUT: &str = r#"
 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -197,14 +167,6 @@ mod tests {
             .map(|machine| machine.min_buttons_joltage())
             .sum::<usize>();
         assert_eq!(answer, 33);
-        Ok(())
-    }
-    #[test]
-    fn part2_min_buttons_seq() -> Result<()> {
-        let machines = parse(INPUT.trim())?;
-        assert_eq!(machines[0].min_buttons_joltage(), 10);
-        assert_eq!(machines[1].min_buttons_joltage(), 12);
-        assert_eq!(machines[2].min_buttons_joltage(), 11);
         Ok(())
     }
 }
